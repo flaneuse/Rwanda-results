266759fae72f86ef1e841f710f96db7dc6e0bfb2
flaneuse <laura.d.hughes@gmail.com>
2017-01-31 12:27:33 -0500

    d3 populate header

--- js/incl-header.js
+++ js/incl-header.js
@@ -1,20 +1,45 @@
-// <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsable-nav" aria-expanded="false">
-//   <span class="sr-only">Toggle navigation</span>
-//   <span class="icon-bar"></span>
-//   <span class="icon-bar"></span>
-//   <span class="icon-bar"></span>
-// </button>
-//
-// <div id="collapsable-nav" class="collapse navbar-collapse">
-//   <ul id="nav-list" class="nav navbar-nav navbar-right">
-//
-//     {% assign allpages = site.pages | sort:'order'%}
-//     {% for page in allpages %}
-//     {% if page.title %}
-//     <li>
-//       <a class="page-link" href="{{ page.url | prepend: site.baseurl }}">{{ page.title }}</a>
-//     </li>
-//     {% endif %}
-//     {% endfor %}
-//   </ul>
-// </div>
+// array of links
+var links = [
+  {name: "introduction", order: 0, link: "/index.html"},
+  {name: "family planning", order: 1, link:"/fp.html"}
+];
+
+// spans to insert to get hamburger collapse on small screens
+var collapse_spans = ["sr-only", "icon-bar", "icon-bar", "icon-bar"];
+
+// select navbar
+var nav = d3.select("#navbar-header")
+
+
+// Hamburger collapse
+nav.append("button")
+  .attr("class", "navbar-toggle collapsed")
+  .attr("data-toggle", "collapse")
+  .attr("data-target", "#collapsable-nav")
+  .attr("aria-expanded", false)
+
+// Create the three bars
+nav.select(".navbar-toggle.collapsed").selectAll("span")
+    .data(collapse_spans)
+  .enter().append("span")
+    .attr("class", function(d) {return d});
+nav.select(".sr-only").text("Toggle navigation")
+
+
+// Create nav bar ul divs.
+nav.append("div")
+  .attr("id", "collapsable-nav")
+  .attr("class", "collapse navbar-collapse")
+.append("ul")
+  .attr("id", "nav-list")
+  .attr("class", "nav navbar-nav navbar-right");
+
+
+// Append data and populate links
+nav.selectAll(".nav.navbar-nav.navbar-right").selectAll("li")
+  .data(links)
+.enter().append("li")
+  .append("a")
+  .attr("class", "page-link")
+  .attr("href", function(d) {return d.link;})
+  .text(function(d) {return d.name;})
